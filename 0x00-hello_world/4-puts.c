c003451988602819f60e76effed44a7eccba112d
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-16 11:19:25 +0000

    added 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - a program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- * return 0
+ * Return: 0
 */
 int main(void)
 {
