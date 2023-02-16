a65c13679b99e89659b895fb34959c40a568a965
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-16 11:54:02 +0000

    added 6-file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints using puts
+ * Return: 0
+*/
+int main(void)
+{
+printf("Size of an char : %lu byte(s)\n",(unsigned long)sizeof(char));
+printf("Size of an int : %lu byte(s)\n",(unsigned long)sizeof(int));
+printf("Size of an long int : %lu byte(s)\n,(unsigned long)sizeof(long int)");
+printf("Size of an long long int : %lu byte(s)\n,(unsigned long)sizeof(long long int)");
+printf("Size of an int : %lu byte(s)\n",(unsigned long)sizeof(float));
+return (0);
+}
