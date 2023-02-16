4781c61978110af1720a8da99a45c384cfca31a8
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-16 12:22:30 +0000

    added 6-file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of an char : %lu byte(s)\n", sizeof(a));
-printf("Size of an int : %lu byte(s)\n", sizeof(b));
-printf("Size of an long int : %lu byte(s))\n", sizeof(c));
-printf("Size of an long long int : %lu byte(s)\n", sizeof(d));
-printf("Size of an float : %lu byte(s)\n", sizeof(f));
+printf("Size of an char: %lu byte(s)\n", sizeof(a));
+printf("Size of an int: %lu byte(s)\n", sizeof(b));
+printf("Size of an long int: %lu byte(s))\n", sizeof(c));
+printf("Size of an long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of an float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
