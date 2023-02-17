80b2c360de3c0ace0d920727ae6ade1d9de6d1c6
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-17 10:58:31 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 /**
  * main - the program display if random number is p,n or 0
- * Return : 0
+ * Return: 0 Success
 */
 int main(void)
 {
 int n;
 srand(time(0));
-n= rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive\n", n);
