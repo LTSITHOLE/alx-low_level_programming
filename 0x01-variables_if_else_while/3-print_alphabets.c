0d091a9d41d02c813f8ad879f4254a859f9b4f46
LTSITHOLE <ltsithole97@gmail.com>
2023-02-18 12:21:45 +0200

    added this file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+/**
+ * main - the program prints alphabets
+ * Return : Always 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n')
+return (0);
+}
