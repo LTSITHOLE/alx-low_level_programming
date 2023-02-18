7461e0bef4c4f089398d90eb59e11060145eb418
LTSITHOLE <ltsithole97@gmail.com>
2023-02-18 12:10:05 +0200

    added this file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
- * main - the program display if random number is p,n or 0
+/**
+ * main - a program that prints the alphabet in lowercase,
  * Return: 0 Success
 */
 int main(void)
 {
 char alp[25] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-for ( i = 0; i < 25; i++)
+for (i = 0; i < 26; i++)
 {
-    putchar(alp[i])
+putchar(alp[i])
 }
 putchar('\n')
 return (0);
