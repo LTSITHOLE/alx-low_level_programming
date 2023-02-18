6ff19199c7e959b75dd6d730d6adedcb86aa422d
LTSITHOLE <ltsithole97@gmail.com>
2023-02-18 11:39:31 +0200

    added file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - the program display if random number is p,n or 0
+ * Return: 0 Success
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negetive\n", n);
+}
+return (0);
+}
