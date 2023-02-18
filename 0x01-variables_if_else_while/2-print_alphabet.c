9c73dd3fabdeeaa39851c5ad2eaef0ebff8d031f
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-18 11:16:21 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - a program that prints the alphabet in lowercase,
  * Return: 0 Success
@@ -7,9 +7,7 @@ int main(void)
 {
 int i;
 for (i = 'a'; i <= 'z'; i++)
-{
 putchar(i);
-}
 putchar('\n');
 return (0);
 }
