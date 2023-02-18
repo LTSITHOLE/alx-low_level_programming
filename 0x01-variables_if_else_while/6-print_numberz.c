799df6f609780af803fceec22aa3abed4a1a934f
LTSITHOLE <ltsithole97@gmail.com>
2023-02-18 12:46:00 +0200

    added this file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+/**
+ * main - a program that prints the alphabet in lowercase,
+ * Return: 0 Success
+*/
+int main(void)
+{
+int ch;
+for (ch = 0; ch <= 10; ch++)
+{
+putchar((ch % 10) + '0');
+}
+putchar('\n');
+return (0);
+}
