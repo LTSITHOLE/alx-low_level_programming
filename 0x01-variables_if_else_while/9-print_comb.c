f887bedba8c74797220c4fbf979c19382a893386
LTSITHOLE <ltsithole97@gmail.com>
2023-02-18 13:09:09 +0200

    added this file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase,
+ * Return: 0 Success
+*/
+int main(void)
+{
+int nm;
+for (nm = 48; nm <= 58; nm++)
+{
+putchar(nm);
+if (nm != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
