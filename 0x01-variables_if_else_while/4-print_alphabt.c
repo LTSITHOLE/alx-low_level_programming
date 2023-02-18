98a52e1314d8f52caf6edc1f144fa36b3df0e5d3
LTSITHOLE <ltsithole97@gmail.com>
2023-02-18 12:34:53 +0200

    added this file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+/**
+ * main - a program that prints the alphabet in lowercase,
+ * Return: 0 Success
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
