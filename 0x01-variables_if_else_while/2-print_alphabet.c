a9a62e1358d75d6d80576896a9b4b6f52dad82a5
LTSITHOLE <ltsithole97@gmail.com>
2023-02-18 12:01:46 +0200

    added this file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+ * main - the program display if random number is p,n or 0
+ * Return: 0 Success
+*/
+int main(void)
+{
+char alp[25] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for ( i = 0; i < 25; i++)
+{
+    putchar(alp[i])
+}
+putchar('\n')
+return (0);
+}
