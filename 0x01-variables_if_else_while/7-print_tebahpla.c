6178345319111ae2f3ab802fbc1bf0403b7ee4cb
LTSITHOLE <ltsithole97@gmail.com>
2023-02-18 12:49:57 +0200

    added this file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+/**
+ * main - a program that prints the alphabet in lowercase,
+ * Return: 0 Success
+*/
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
