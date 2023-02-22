970fb48e29d10e5204f2d853966a6b27c561f448
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-21 18:40:14 -0800

    Create 2-main.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - the program display if random number is p,n or 0
+ * print_alphabet print all alphabet from a to z
+ * Return: 0 Success
+*/
+int main(void)
+{
+print_alphabetx10();
+_putchar('\n');
+return (0);
+}
