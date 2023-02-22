be36bc737b2a2e6d39fe27a11307dc2a9aca7195
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-21 19:13:20 -0800

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 * _islower - writes character to stdout in the program
-* @c - this is the character to print from input
-* Return - 1 if c is lowercase otherwise 0 according to input
+* @c: this is the character to print from input
+* Return: 1 if c is lowercase otherwise 0 according to input
 */
 int _islower(int c)
 {
