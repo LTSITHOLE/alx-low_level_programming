954254f575606d23f8fa6bc17eef5a5cd46e9c39
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-21 18:39:10 -0800

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabetx10 - writes character to stdout in the program
+* @c -the character to print
+* Return - 1 Sucess
+* on error - -1 is returned
+*/
+void print_alphabetx10(void)
+{
+char x;
+int x;
+for (x =0; x < 10; x++)
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
