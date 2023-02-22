3d910e4bf2aa401384d642d70633aef7b3cfa59c
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-21 18:32:05 -0800

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _print_alphabet - writes character to stdout in the program
+* print_alphabet - writes character to stdout in the program
 * @c -the character to print
 * Return - 1 Sucess
 * on error - -1 is returned
