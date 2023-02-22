d64fca2702db11ffdc1d34f1beb9d0c962e947fb
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-21 18:04:19 -0800

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _putchar - writes character to stdout
+* @c -the character to print
+* Return - 1 Sucess
+* on error - -1 is returned
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
