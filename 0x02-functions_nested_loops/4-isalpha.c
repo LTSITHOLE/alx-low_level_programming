1e13900da61681a3a6874a841128a4d9b9b3941b
LTSITHOLE <57663074+LTSITHOLE@users.noreply.github.com>
2023-02-21 19:17:56 -0800

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _islower - writes character to stdout in the program
+* @c: this is the character to print from input
+* Return: 1 if c is lowercase otherwise 0 according to input
+*/
+int _islower(int c)
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
+else
+return (0);
+}
